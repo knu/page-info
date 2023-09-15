253b9ab2d76e610442fed02dcdba7fdd44642958
Akinori MUSHA <knu@idaemons.org>
2023-09-15 23:59:12 +0900

    Obtain icon and og:site_name as part of PageInfo

--- src/getPageInfo.ts
+++ src/getPageInfo.ts
@@ -3,20 +3,30 @@ export type PageInfo = {
   canonicalUrl?: string | null;
   title?: string | null;
   description?: string | null;
+  icon?: string | null;
+  og_site_name?: string | null;
   og_title?: string | null;
   og_description?: string | null;
   og_image?: string | null;
 };
 
 export const getPageInfo: () => PageInfo = () => {
-  const getElementAttribute = (selector: string, attribute: string) => {
-    const value = document
-      .querySelector(`${selector}[${attribute}]`)
-      ?.getAttribute(attribute);
+  const getElementAttribute = (
+    selector: string,
+    attribute: string,
+    maxBy?: (element: Element) => any,
+  ) => {
+    const aSelector = `${selector}[${attribute}]`;
+
+    const value = maxBy
+      ? Array.from(document.querySelectorAll(aSelector))
+        .map((element) => [maxBy(element), element.getAttribute(attribute)])
+        .sort(([a], [b]) => (a < b ? 1 : a > b ? -1 : 0))[0]?.[1]
+      : document.querySelector(aSelector)?.getAttribute(attribute);
 
     if (!value) return null;
 
-    const div = document.createElement('div');
+    const div = document.createElement("div");
     div.innerHTML = value;
     return div.textContent!;
   };
@@ -24,10 +34,29 @@ export const getPageInfo: () => PageInfo = () => {
   const result: PageInfo = {
     url: window.location.href,
     canonicalUrl: getElementAttribute("link[rel='canonical']", "href"),
+    icon: getElementAttribute(
+      "link[rel='icon']",
+      "href",
+      (elem) =>
+        elem
+          .getAttribute("sizes")
+          ?.split(/\D+/)
+          .map((s) => parseInt(s))
+          .filter((n) => !isNaN(n))
+          .sort()
+          .pop() ?? 0,
+    ),
     title: document.title,
     description: getElementAttribute("meta[name='description']", "content"),
     og_title: getElementAttribute("meta[property='og:title']", "content"),
-    og_description: getElementAttribute("meta[property='og:description']", "content"),
+    og_site_name: getElementAttribute(
+      "meta[property='og:site_name']",
+      "content",
+    ),
+    og_description: getElementAttribute(
+      "meta[property='og:description']",
+      "content",
+    ),
     og_image: getElementAttribute("meta[property='og:image']", "content"),
   };
 
